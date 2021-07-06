ebee91d369f8544b7560358331268ab4341c0767
latoyadawson <78005063+latoyadawson@users.noreply.github.com>
2021-07-05 22:37:29 -0700

    schema files and server set up

--- server/server.js
+++ server/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const { authMiddleware } = require('./utils/auth');
+
+//import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
+//imprt typeDefs and resolvers 
+const { typeDefs, resolvers } = require('./schemas');
+
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//create a new apollo server and pass in our schema data 
+const server = new ApolloServer({ 
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+//integrate Apollo server with Express application as middleware
+server.applyMiddleware({app});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +32,22 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+//if we make a GET request to any location on the server that doesn't have an explicit route defined, respond with the production-ready React front-end code.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
+  });
 });
+
+process.on('uncaughtException', function(err) {
+  console.log('Caught exception: ' + err);
+});;
